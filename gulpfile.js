ee9a6f80e00537ad58db406b6e881e6bb25f8a20
Andrea Crawford <andrea.crawford13@gmail.com>
2017-10-18 01:32:02 +0200

    Add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('build', function(){
+gulp.task('serve', ['css','html'], function() {
+    browserSync.init({
+        server: "dist"
+    });
+
+    gulp.watch('src/**', ['css','html']);
+    gulp.watch("dist/index.html").on('change', browserSync.reload);
+});
+
+gulp.task('css', function(){
+    gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('html', function(){
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('build', ['html','css'], function(){
     gulp.src('node_modules/knockout/build/output/knockout-latest.js')
         .pipe(rename('knockout.js'))
         .pipe(gulp.dest('dist/js/vendor/'));
 });
 
-gulp.task('build:dev', function(){
+gulp.task('build:dev', ['html','css'], function(){
     gulp.src('node_modules/knockout/build/output/knockout-latest.debug.js')
         .pipe(rename('knockout.js'))
         .pipe(gulp.dest('dist/js/vendor/'));
